16b9868657b8014625a2d9d88cf5981ced412ee9
Asma Samadi <asmasamadi001@gmail.com>
2026-02-11 23:47:40 +0430

    created models and made migrations

--- empProject/empApp/models.py
+++ empProject/empApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Position(models.Model):
+    title=models.CharField(max_length=50)
+
+class Employee(models.Model):
+    fullName=models.CharField(max_length=50)
+    mobile=models.CharField(max_length=50)
+    email=models.EmailField()
+    position=models.ForeignKey(Position,on_delete=models.CASCADE)
