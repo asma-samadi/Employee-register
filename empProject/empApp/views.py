4b90c1b7e482ca3e3ada7d63ca295bcafbe03cec
Asma Samadi <asmasamadi001@gmail.com>
2026-02-12 00:01:11 +0430

    worked on urls and views

--- empProject/empApp/views.py
+++ empProject/empApp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+def employeeList(request):
+    render
+
+def employeeForm(request):
+    render
+
+def employeeDelete(request):
+    render
+    
