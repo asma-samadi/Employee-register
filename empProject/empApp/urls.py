ab3b1585302052be733ca36ea19ab64f03fe5b3b
Asma Samadi <asmasamadi001@gmail.com>
2026-02-13 00:46:43 +0430

    added more buttens and worked on update and delete operations

--- empProject/empApp/urls.py
+++ empProject/empApp/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.employeeForm, name='employeeForm'),
+    path('<int:id>/', views.employeeForm, name='employeeUpdate'),
+    path('delete/<int:id>/', views.employeeDelete, name='employeeDelete'),
     path('list/', views.employeeList, name='employeeList'),
 ]  
+
--- empProject/empApp/views.py
+++ empProject/empApp/views.py
@@ -2,23 +2,32 @@ from django.shortcuts import render, redirect
 from .forms import Employee_form
 from .models import Employee
 
-# Create your views here.
+# Create your views here
 
+def employeeForm(request, id=0):
+    if request.method == 'GET':
+        if id == 0:
+            form = Employee_form()
+        else:
+            employee = Employee.objects.get(pk=id)
+            form = Employee_form(instance=employee)
+
+        return render(request, 'empApp/EmployeeForm.html', {'form':form})
+    else: 
+        if id == 0:
+            form = Employee_form(request.POST)
+        else:
+            employee = Employee.objects.get(pk=id)
+            form = Employee_form(request.POST,instance=employee)
+        if form.is_valid():
+            form.save()
+        return redirect('/employee/list')
+    
 def employeeList(request):
     employees = Employee.objects.all()
     return render(request, 'empApp/employeeList.html', {'employeeList': employees})
 
-
-def employeeForm(request):
-    if request.method == 'GET':
-        form = Employee_form()
-        return render(request, 'empApp/EmployeeForm.html', {'form':form})
-    else: 
-        form = Employee_form(request.POST)
-        if form.is_valid():
-            form.save()
-        return redirect('/employee/list')
-
-def employeeDelete(request):
-    render
-    
+def employeeDelete(request,id):
+    employee = Employee.objects.get(pk=id)
+    employee.delete()
+    return redirect('/employee/list')
